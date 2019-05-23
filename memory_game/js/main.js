86ded1cf9efb3c377bc169153ce0c35d6bd80223
Sian Alcock <sian.alcock@lawena.org>
2019-05-23 11:44:11 +0100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!")
+
+//Declare variables for each card and assign the value of "Queen" to two cards and "King" to the other two cards
+
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
